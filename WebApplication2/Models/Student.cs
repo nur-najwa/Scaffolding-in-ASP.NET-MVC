2308ab93c3f4934fca38669348efe8911ec4e3ec
Najwa <najwa@iris.com.my>
2023-01-27 15:20:26 +0800

    first commit

---
+++ WebApplication2/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Student
+    {
+        public string id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+    }
+}
